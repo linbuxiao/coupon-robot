3c604349fc34f7ac862dd2cdecf588c09be87d52
linbuxiao <linbuxiao@gmail.com>
2021-10-12 22:57:34 +0800

    debug: env

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const res = await got({
   url: meituanUrl,
   headers: {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63040026)',
-    'Cookie': MT_COOKIE
+    // 'Cookie': MT_COOKIE
+    'Cookie': '_lxsdk_cuid=17c74bdb921c8-0349d6b893507f-61263e23-100200-17c74bdb9217d; unpl=v1_NuqFe7fJwsTLpDwOohrDbG-feb_JcCEPuu-QaxTMui3TUFrLC0TEu_miyWP-twik; us=wandie; ut=71023.163404616411.91345217.2; ud=; iuuid=58A7540CF90EF64A2C78D71F195A7DA7CCE1C2B54B698B684AB748012B7467EF; mt_c_token=DCF2J_mm6BFstp4ywxHZPuutFeoAAAAA4g4AAEgdgKSMyoQ4UrCYOkp6fI9gW5R_iSi4ihTKVc5zfzLrKSKgEHwmEliP4UuqzNDYpQ; thirdlogin_token=DCF2J_mm6BFstp4ywxHZPuutFeoAAAAA4g4AAEgdgKSMyoQ4UrCYOkp6fI9gW5R_iSi4ihTKVc5zfzLrKSKgEHwmEliP4UuqzNDYpQ; oops=DCF2J_mm6BFstp4ywxHZPuutFeoAAAAA4g4AAEgdgKSMyoQ4UrCYOkp6fI9gW5R_iSi4ihTKVc5zfzLrKSKgEHwmEliP4UuqzNDYpQ; _lx_utm=utm_term%3D%26utm_campaign%3Dother%26utm_medium%3DMU%26utm_source%3D60413%26utm_content%3D1380426725749538828; _lxsdk=58A7540CF90EF64A2C78D71F195A7DA7CCE1C2B54B698B684AB748012B7467EF; _lxsdk_s=17c74bdb92a-29d-f9d-ea5%7C%7C30'
   },
   json: {
     "gundamId": MT_GUNDAM
